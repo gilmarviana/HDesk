20baa6f654d13501ec8beb6f704f07c26f780bd1
gilmarviana <gil.gmviana@gmail.com>
2023-01-29 16:24:00 -0300

    projeto iniciado

---
+++ src/main/java/com/gmviana/HDesk/HDeskApplication.java
@@ -0,0 +1,13 @@
+package com.gmviana.HDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmviana/HDesk/HDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmviana.HDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
